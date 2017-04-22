8b6e20630716b81ce78e7a3886551e8b455afe6b
Vladimir Starkov <iamstarkov@gmail.com>
2017-04-22 02:21:26 +0200

    feat: withTheme through onThemUpdate

--- src/withTheme.js
+++ src/withTheme.js
@@ -1,35 +1,8 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {CHANNEL} from './ThemeProvider'
+import onThemeUpdate from './onThemeUpdate'
 
-const withTheme = Component => class extends React.Component {
-  static contextTypes = {
-    [CHANNEL]: PropTypes.func,
-  };
-
-  // state: { theme?: ?Object } = {};
-  // unsubscribe: () => void;
-
-  componentWillMount() {
-    if (!this.context[CHANNEL]) {
-      throw new Error('[withTheme] Please use ThemeProvider to be able to use withTheme')
-    }
-
-    const subscribe = this.context[CHANNEL]
-    this.unsubscribe = subscribe((theme) => {
-      this.setState({theme})
-    })
-  }
-
-  componentWillUnmount() {
-    if (typeof this.unsubscribe === 'function') this.unsubscribe()
-  }
-
-  render() {
-    const {theme} = this.state
-
-    return <Component theme={theme} {...this.props} />
-  }
-}
+const withTheme = Component => onThemeUpdate((theme, props) => {
+  return <Component theme={theme} {...props} />
+})
 
 export default withTheme
